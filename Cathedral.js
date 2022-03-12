98d541767fe14b0968d4c78107a3aa00768eb46e
dom111 <dom111@users.noreply.github.com>
2022-03-12 21:07:59 +0000

    Initial commit.

---
+++ Cathedral.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cathedral = void 0;
+const CityImprovement_1 = require("@civ-clone/core-city-improvement/CityImprovement");
+class Cathedral extends CityImprovement_1.default {
+}
+exports.Cathedral = Cathedral;
+exports.default = Cathedral;
+//# sourceMappingURL=Cathedral.js.map
---
+++ README.md
@@ -0,0 +1,4 @@
+# base-city-improvement-cathedral
+
+This is a template repo.
+
